9042891aa7c6d938fdaab3f032330a8287e794ea
Thomas <thomascode92@hotmail.com>
2026-08-13 21:20:02 +0200

    chore: setting up unity and rider
    Test the Unity and Rider integration:
      - configure rider as script editor
      - add a gameobject to the scene
      - create monobehaviour script, attach to gameobject
      - introduction to methods, by rotating the gameobject

---
+++ Assets/Driver.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        transform.Rotate(0, 0, 45);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
