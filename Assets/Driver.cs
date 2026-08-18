b2f6004077df4cd5a6243d348df8e1bc988cdf48
Thomas <thomascode92@hotmail.com>
2026-08-18 12:36:10 +0200

    chore: transform.Translate()
    Use the `transform.Translate()` method to move a GameObject.

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -5,11 +5,12 @@ public class Driver : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
-        transform.Rotate(0, 0, 45);
     }
 
     // Update is called once per frame
     private void Update()
     {
+        transform.Rotate(0, 0, .1f);
+        transform.Translate(0, .01f, 0);
     }
 }
